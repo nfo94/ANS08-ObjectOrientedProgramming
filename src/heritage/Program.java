f4841883e20dc985946b732ebfe3259ff400db3c
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2021-05-01 21:38:53 -0300

    upcasting and downcastig

--- src/heritage/Program.java
+++ src/heritage/Program.java
@@ -6,5 +6,13 @@ public class Program {
     BusinessAccount bacc = new BusinessAccount(1002, "Marcelo Oliveira", 500.0);
 
 //    UPCASTING
+    Account acc1 = bacc;
+    Account acc2 = new BusinessAccount(1003, "Bob", 200.0);
+    Account acc3 = new SavingsAccount(1000, "Anna", 0.0, 0.01);
+
+//    DOWNCASTING
+    BusinessAccount acc4 = (BusinessAccount)acc2;
+//    ERROR!
+//    BusinessAccount acc5 = (BusinessAccount)acc3;
   }
 }
